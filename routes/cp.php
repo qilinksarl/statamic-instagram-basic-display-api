f4687cf7824f8826119f3a1fdc4676cbe2500e46
Qilink <42271362+qilinksarl@users.noreply.github.com>
2023-06-06 10:47:16 +0200

    Update cp.php

--- routes/cp.php
+++ routes/cp.php
@@ -1,5 +1,8 @@
 <?php
 
+use NineteenSquared\Instagram\Http\Controllers\InstagramLoginController;
+use NineteenSquared\Instagram\Http\Controllers\InstagramLogoutController;
+
 Route::prefix('nineteen-ig/')->name('nineteen-ig.')->group(function () {
     Route::get('/', 'InstagramLoginController@index')->name('index');
     Route::get('/logout', 'InstagramLogoutController')->name('logout');
